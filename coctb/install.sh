9a0257c9056e15359b7b79f1d0910bd5745b10a6
mohsan <mshami668@gmail.com>
2022-02-11 12:10:55 +0500

    Updated the ReadME

---
+++ coctb/install.sh
@@ -0,0 +1,10 @@
+
+#update the packages
+sudo apt update
+#installing the prerequist
+sudo apt-get install make gcc g++ python3 python3-dev python3-pip
+#installing the cocotb
+pip install cocotb
+#installing the gtkwave
+sudo apt install gtkwave
+sudo apt-get install verilog
